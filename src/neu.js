71639fcbab2f76792b99a37b218f80a9f92aafaa
mawngo <sitdownrightnow.dev@gmail.com>
2024-06-23 15:19:03 +0700

    feat: re-design home page and guide page

--- src/neu.js
+++ src/neu.js
@@ -2,13 +2,19 @@ import { app as a, events as e, init, os, window as w } from "@neutralinojs/lib"
 
 try {
     init();
+    const quit = document.getElementById("quit");
+    quit.addEventListener("click", async function() {
+        if (window.Neu) {
+            await window.Neu.app.exit();
+        }
+    });
+    quit.style.removeProperty("display");
     window.Neu = {
         window: w,
         app: a,
         events: e,
         os: os
     };
-    await window.Neu.window.focus();
     console.log("Running in desktop");
 } catch (e) {
     console.log("Running in browser");
