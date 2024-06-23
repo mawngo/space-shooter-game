94f54dd2e41cb940922412177b100a0d26438fec
mawngo <sitdownrightnow.dev@gmail.com>
2024-06-23 17:17:16 +0700

    chore: re-balance game config

--- src/config.js
+++ src/config.js
@@ -14,12 +14,12 @@ export function loadConfig() {
         asteroid: {
             spin: 0.3,
             damagePerRadius: 0.25,
-            childrenSizeRatio: 0.6,
-            minimalSizeCanSplit: 28,
-            minNumberOfChild: 2,
-            maxNumberOfChild: 2,
+            childrenSizeRatio: 0.65,
+            minimalSizeCanSplit: 30,
+            minNumberOfChild: 1,
+            maxNumberOfChild: 3,
             minSize: 60,
-            maxSize: 80,
+            maxSize: 100,
             minSpeed: 0,
             maxSpeed: 5
         },
@@ -30,8 +30,8 @@ export function loadConfig() {
             size: 20
         },
         ammo: {
-            damage: 8,
-            destroyAsteroidRate: 0.6,
+            damage: 10,
+            destroyAsteroidRate: 0.55,
             speed: 10,
             size: 25
         },
@@ -44,22 +44,22 @@ export function loadConfig() {
             item0: {
                 name: "Blue Ammo",
                 amount: 3,
-                rate: 0.20
+                rate: 0.2
             },
             item1: {
                 name: "Burger",
-                heal: 20,
-                rate: 0.20
+                heal: 15,
+                rate: 0.2
             },
             item2: {
                 name: "Red Ammo",
                 amount: 1,
-                rate: 0.05
+                rate: 0.03
             },
             item3: {
                 name: "Antimatter Bomb",
                 amount: 1,
-                rate: 0.03
+                rate: 0.02
             },
             item4: {
                 name: "Gold",
