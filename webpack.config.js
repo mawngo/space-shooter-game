54f7bba16712482c96cad3aec65622fc615d8e0a
mawngo <sitdownrightnow.dev@gmail.com>
2025-05-03 19:37:30 +0700

    chore: ignore minified js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, "resources"),
-        filename: "[name].js"
+        filename: "[name].min.js"
     },
     experiments: {
         topLevelAwait: true
