4ad62d45ad2a148941b11f5477dfeb59e6db3799
mawngo <sitdownrightnow.dev@gmail.com>
2025-05-05 15:55:09 +0700

    feat: allow config gameVersion and gameControlMode from params

--- src/init.js
+++ src/init.js
@@ -1,9 +1,7 @@
 import { app as a, events as e, init, os, window as w } from "@neutralinojs/lib";
 
-window.localStorage.setItem("gameVersion", window.gameVersion || VERSION);
-if (typeof window.gameControlMode == "string") {
-    window.localStorage.setItem("gameControlMode", window.gameControlMode);
-}
+window.localStorage.setItem("gameVersion", getParam("gameVersion") || VERSION);
+window.localStorage.setItem("gameControlMode", getParam("gameControlMode"));
 document.addEventListener("gameQuit", () => {
     if (window.game) {
         window.game.stop();
@@ -11,6 +9,13 @@ document.addEventListener("gameQuit", () => {
     if (window.gameQuit) {
         window.gameQuit();
     }
+});
+document.querySelectorAll("a[href]").forEach(a => {
+    if (!a.href.includes("?")) {
+        a.href = a.href + window.location.search;
+        return;
+    }
+    a.href = a.href + "&" + window.location.search.substring(1);
 });
 
 try {
@@ -39,12 +44,43 @@ try {
 // Setup quit button.
 if (window.gameQuit) {
     const quit = document.getElementById("quit");
-    if (!quit) {
-        return;
+    if (quit) {
+        quit.addEventListener("click", async function() {
+            document.dispatchEvent(new CustomEvent("gameQuit", { bubbles: true }));
+        });
+        quit.style.removeProperty("display");
     }
-    quit.addEventListener("click", async function() {
-        document.dispatchEvent(new CustomEvent("gameQuit", { bubbles: true }));
-    });
-    quit.style.removeProperty("display");
 }
 
+
+function getParam(name, from = ["search", "window"]) {
+    let param = "";
+    if (from.includes("search")) {
+        param = new URLSearchParams(window.location.search).get(name);
+        if (param) return param;
+    }
+
+    if (from.includes("window")) {
+        param = window[name];
+        if (param) return param;
+    }
+
+    if (from.includes("referrer") && document.referrer) {
+        param = new URL(document.referrer).searchParams.get(name);
+        if (param) return param;
+    }
+
+    if (from.includes("localStorage")) {
+        param = localStorage.getItem(name);
+        if (param) return param;
+    }
+
+    if (from.includes("cookies")) {
+        param = (() => {
+            const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
+            if (match) return match[2];
+        })();
+        if (param) return param;
+    }
+    return "";
+}
