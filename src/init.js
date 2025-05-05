d54cb433aff6e05e124528c2ba99710aa1ac0858
mawngo <sitdownrightnow.dev@gmail.com>
2025-05-05 15:36:12 +0700

    feat: fix quit not working on guide screen, allow hiding part of the guides

--- src/init.js
+++ src/init.js
@@ -1,6 +1,17 @@
 import { app as a, events as e, init, os, window as w } from "@neutralinojs/lib";
 
-window.localStorage.setItem("version", VERSION);
+window.localStorage.setItem("gameVersion", window.gameVersion || VERSION);
+if (typeof window.gameControlMode == "string") {
+    window.localStorage.setItem("gameControlMode", window.gameControlMode);
+}
+document.addEventListener("gameQuit", () => {
+    if (window.game) {
+        window.game.stop();
+    }
+    if (window.gameQuit) {
+        window.gameQuit();
+    }
+});
 
 try {
     init();
@@ -15,29 +26,25 @@ try {
         window.gameQuit = () => {
             window.Neu.app.exit();
         };
-        window.Neu.events.on("windowClose", () => {
-            document.dispatchEvent(new CustomEvent("gameQuit"));
-        });
     }
+    window.Neu.events.on("windowClose", () => {
+        document.dispatchEvent(new CustomEvent("gameQuit"));
+        console.log("quit");
+    });
     console.log("Running in desktop");
 } catch (e) {
     // Ignore.
 }
 
-
-// Setup quit event.
+// Setup quit button.
 if (window.gameQuit) {
     const quit = document.getElementById("quit");
+    if (!quit) {
+        return;
+    }
     quit.addEventListener("click", async function() {
         document.dispatchEvent(new CustomEvent("gameQuit", { bubbles: true }));
     });
     quit.style.removeProperty("display");
 }
-document.addEventListener("gameQuit", () => {
-    if (window.game) {
-        window.game.stop();
-    }
-    if (window.gameQuit) {
-        window.gameQuit();
-    }
-});
+
